ab5d9a60c4dab18362c65e04e0a51a11e08f7629
KallistoGanymed <obsidian42@outlook.de>
2024-12-30 13:32:00 +0100

    intellij

---
+++ klasse.java
@@ -0,0 +1,8 @@
+public class klasse {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 3;
+        int c = a +b;
+        System.out.println("Summe von a und b: " + c);
+    }
+}
